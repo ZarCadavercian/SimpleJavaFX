a1b1dc9951b141067a03731ddac8fddeff29df49
ZarCadavercian <drakoniy@ua.fm>
2015-02-04 16:39:08 -0500

    Updated pom.xml; Moved sample.fxml to resources folder..

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Simple JavaFX Application");
         primaryStage.setScene(new Scene(root, 300, 275, Color.LIGHTGREY));
         primaryStage.show();
